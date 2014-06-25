4d4a6861f446d4eb701fcf82a31bd51f3077565f
broue004 <ewan.broughton@amido.com>
2014-06-25 14:31:24 +0100

    created config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'amido-perf-test');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'b7f01ba1b08ed4');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'dafd53d0');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'eu-cdbr-azure-north-b.cloudapp');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'yqSkg^x,kFDx_0|D-?=dyd#85=33G<=6WwWxQip{]:l8C9[M;,>,`<SG70Ko4hjx');
+define('SECURE_AUTH_KEY',  '$|sIAt+ Mh<_z$091|CqWq=Z+71% BEj+cN0Na+9%sV?06J_^CH0DAM=dtetMFBl');
+define('LOGGED_IN_KEY',    '&B|hZ@mLqcuk7DVV7cWQ`j04kkVI|SqROGeiu3#q<tDYeXV9}*;E8y%&>R4Dzn4 ');
+define('NONCE_KEY',        'yHa/+>3i3#%X`gg;Va|1A-3q|a4z.qqD?0;=D:%9[O$:aXDs`43d11;*$x(xdhij');
+define('AUTH_SALT',        '19ugvULd7Dl@z9Cw]>xc^2/=Q,F4e}0}qI5Gpv_AsEegP^g)8Iilo$>CL<&-v&+6');
+define('SECURE_AUTH_SALT', ']@tFRG6|NYsqSw;=m1IG(EWPN6at^czSe+vsfHnR.<n@e3Che+d!vm5gF}![1|MZ');
+define('LOGGED_IN_SALT',   'Tp3/A^<^?KQO}:*:* [U`z_$|tF||NV9-)VvfsT@.NKFARJr]~BlMVLFS+J 55AS');
+define('NONCE_SALT',       'WBsylH!9SyF7X-&{nznwe|qxq7}L(t1|dr%csjxUS%deAoq_-75!sgU(u WI)G#Q');
 
 /**#@-*/
 
